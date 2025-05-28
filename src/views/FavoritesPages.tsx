e3643708e2630741692a77650134818b91e4badf
AryDaniel <ary.lopez@cetis107.edu.mx>
2025-05-27 23:57:39 -0700

    feat(favorites): enhance FavoritesPages component with conditional rendering and styling

--- src/views/FavoritesPages.tsx
+++ src/views/FavoritesPages.tsx
@@ -1,9 +1,28 @@
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
 
 export default function FavoritesPages() {
+  const favorites = useAppStore((state) => state.favorites)
+  const hasFavorites = useMemo(() => favorites.length,[favorites])
   return (
     <>
-      <h1>Favorites</h1>
-    
+      <h1 className="text-6xl font-extrabold">Favorites</h1>
+      {hasFavorites ?(
+          <div className="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-3 my-10 gap-10">
+            {favorites.map(drink => (
+              <DrinkCard
+                key={drink.idDrink}
+                drink={drink}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="my-10 text-center text-2xl">
+            The favorite list is empty, add a recipe to favorites
+          </p>
+        )
+      }
     </>
   )
 }
