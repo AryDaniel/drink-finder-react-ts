14eaf70b4dc980b77fefa9b73d280b344d749708
AryDaniel <ary.lopez@cetis107.edu.mx>
2025-05-27 20:07:29 -0700

    feat(favorites): add favorites slice and integrate with app store

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,9 @@
 import { create } from "zustand";
 import { createRecipesSlice, type recipeSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import { type FavoritesSliceType, createFavoriteSlice } from "./favoritesSlice";
 
-export const useAppStore = create<recipeSliceType>()(devtools((...a) => ({
-    ...createRecipesSlice(...a)
+export const useAppStore = create<recipeSliceType & FavoritesSliceType>()(devtools((...a) => ({
+    ...createRecipesSlice(...a),
+    ...createFavoriteSlice(...a),
 })))
