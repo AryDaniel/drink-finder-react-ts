c9b663f88ff35418e5133cf9756ecea8f8eaa462
AryDaniel <ary.lopez@cetis107.edu.mx>
2025-05-29 22:27:06 -0700

    feat(ai): create AISlice for recipe generation and integrate into useAppStore

---
+++ src/stores/aiSlice.ts
@@ -0,0 +1,13 @@
+import { type StateCreator } from "zustand";
+
+export type AISlice = {
+    recipe: string
+    generateRecipe: (promp: string) => Promise<void>
+}
+
+export const createAISlice : StateCreator<AISlice, [], [], AISlice> = () => ({
+    recipe: '',
+    generateRecipe: async (prompt) => {
+        console.log(prompt)
+    }
+})
