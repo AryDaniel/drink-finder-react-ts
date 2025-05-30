dfddbc29d1bc1ebd56937472a8528bfb608297f8
AryDaniel <ary.lopez@cetis107.edu.mx>
2025-05-29 23:54:51 -0700

    feat(ai): implement AI recipe generation with AIService and update GenerateAI component

---
+++ src/services/AIService.ts
@@ -0,0 +1,14 @@
+// npm i ai
+import { streamText } from "ai"
+import { openrouter } from "../lib/ai"
+
+export default {
+    async generateRecipe(prompt:string) {
+        const result = streamText({
+            model: openrouter('meta-llama/llama-3.3-70b-instruct:free'),
+            prompt: prompt
+        })
+
+        return result.textStream
+    }
+}
